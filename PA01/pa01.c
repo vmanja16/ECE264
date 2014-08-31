4c47f8a5b479e7338e80d7e9d6cadaf6b8810426
Vikram Kajadi Manja <vmanja@ecegrid-thin1.ecn.purdue.edu>
2014-08-30 22:33:23 -0400

    start homework

--- PA01/pa01.c
+++ PA01/pa01.c
@@ -1,78 +1,106 @@
-/* 
- * Do not modify this file.
- */
-#include "pa01.h"
+ 
 #include <stdio.h>
 #include <stdlib.h>
-#include <libgen.h>
-#include <string.h>
 
-static void printUsage(char * exec_path)
+// This includes the /definitions/ of the functions that you write
+// in this assignment. Including the definitions allows you to use
+// the functions in this file.
+#include "answer01.h"
+
+void printArray(int * array, int len)
 {
-    const char * exec_name = basename(exec_path);
-    printf("\n"
-	   "   Usage: %s <filename>\n"
-	   "\n"
-	   "   Input files are in the \"inputs\" directory. Thus you can\n"
-	   "   try something like:\n"
-	   "\n"
-	   "      aturing > ./%s inputs/input0\n"
-	   "\n"
-	   "   and you should get something like:\n"
-	   "\n"
-	   "      sum = -3772.\n"
-	   "      The array has 54 negative elements.\n"
-	   "      The elements are not increasing.\n"
-	   "\n", 
-	   exec_name, exec_name);
+    printf("{");
+    int ind;
+    for(ind = 0; ind < len; ++ind) {
+	printf("%d", array[ind]);
+	if(ind != len - 1) {
+	    printf(", ");
+	}
+    }
+    printf("}");
+    // If we don't include a '\n' character, then we need to 
+    // include this line to ensure that our output is immediately
+    // printed out output.
+    fflush(stdout); 
+}
+
+void test_arraySum(int * array, int len, int expected)
+{
+    printArray(array, len);
+    int sum = arraySum(array, len);
+    printf(". sum = %d, expected = %d.", sum, expected);
+    if(sum != expected)
+	printf(" FAIL");
+    printf("\n");
+}
+
+void test_00_arraySum()
+{
+    printf("Testing arraySum(...)\n");
+
+    // Here we use "static initialization" to create an array
+    int array1[] = { -4, -1, 0, 1, 5, 10, 20, 21 };
+    int len1 = 8;
+    int expected1 = 52;
+    test_arraySum(array1, len1, expected1);
+
+    // Our functions must always work... even on empty arrays
+    int array2[] = {};
+    int len2 = 0;
+    int expected2 = 0;
+    test_arraySum(array2, len2, expected2);
+
+    // You can add more test-cases here
+    // ...
+
+    printf("\n"); // Tidy output is easier to use
+}
+
+void test_arrayCountNegative(int * array, int len, int expected)
+{
+    printArray(array, len);
+    int count = arrayCountNegative(array,len); 
+    printf(". count = %d, expected = %d.", count, expected);
+    if(count != expected)
+	printf(" FAIL");
+    printf("\n");
+}
+
+void test_01_arrayCountNegative()
+{
+    printf("Testing arrayCountNegative(...)\n");
+
+    int array1[] = { 1, 4, -5, 4 };
+    int len1 = 4;
+    test_arrayCountNegative(array1, len1, 1);
+
+    int array2[] = {};
+    int len2 = 0;
+    test_arrayCountNegative(array2, len2, 0);
+
+    int array3[] = { 0, 1, 2, 3, 4 };
+    int len3 = 5;
+    test_arrayCountNegative(array3, len3, 0);
+
+    int array4[] = { -1, -2, -3, -4 };
+    int len4 = 4;
+    test_arrayCountNegative(array4, len4, 4);
+
+    // You can add more test-cases here
+    // ...
+
+    printf("\n");
 }
 
 int main(int argc, char * * argv)
 {
-    if (argc != 2)
-	{
-	    printUsage(argv[0]);
-	    return EXIT_FAILURE;
-	}
-    if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
-	{
-	    printUsage(argv[0]);
-	    return EXIT_SUCCESS;
-	}
-    FILE * fptr = fopen(argv[1], "r");
-    if (fptr == NULL)
-	{
-	    fprintf(stderr, "Failed to open file '%s', aborting!\n", argv[1]);
-	    return EXIT_FAILURE;
-	}
-    int length = 0;
-    int value;
-    while (fscanf(fptr, "%d", & value) == 1)
-	{
-	    length ++;
-	}
-    fseek (fptr, 0, SEEK_SET);
-    int * arr = malloc(length * sizeof(int));
-    length = 0;
-    while (fscanf(fptr, "%d", & (arr[length])) == 1)
-	{
-	    // printf("arr[%d] = %d\n", length, arr[length]);
-	    length ++;
-	}
-    int sum = addElement(arr, length);
-    int neg = countNegative(arr, length);
-    int inc = isIncreasing(arr, length);
-    printf("sum = %d.\n", sum);
-    printf("The array has %d negative elements.\n", neg);
-    printf("The elements are ");
-    if (inc == 0)
-	{
-	    printf("not ");
-	}
-    printf("increasing.\n");
-    printf("\n");
-    free (arr);
-    fclose (fptr);
+    printf("Welcome to ECE264, we are working on PA01.\n\n");
+
+    // Uncomment to run two example (incomplete) testcases.
+    test_00_arraySum();
+    test_01_arrayCountNegative();
+
     return EXIT_SUCCESS;
 }
+
 
