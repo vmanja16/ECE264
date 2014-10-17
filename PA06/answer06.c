d95a710bef128fbd3584e0aaedc37319fd999b03
Vikram Kajadi Manja <vmanja@ecegrid-thin1.ecn.purdue.edu>
2014-10-17 19:47:01 -0400

    ACTUALLY DONE WITH PA06

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -3,33 +3,67 @@
 
 void print_directions(char** maze, int w, int h) 
 
-
-
 {
 
+ printf("w = %d, h = %d... hopefullR 9 and 7\n", w, h);
 char ** Maze = maze; //Assigning Maze to maze
-char  *Current = &Maze[0][0]; // Assigning Character pointer to left corner
 
 /*-----------------FINDING ENTRANCE--------------------*/
 
-    int I =0;
+    int I = 0, J = h-1, EC = 0, ER = 0;
     
-
-    while ( Maze[0][I] == 'X') 
+    printf("%d,%d\n", EC, ER);
+    I = 0;
+    J = 0;
+    while ( Maze[J][I] == 'X') 
         {
-   			I++;    	
+   			I++;    	// Entrance C Coordinate
         }
-    Current = &Maze[0][I]; // Current points to opening of maze
 
+/*---------------------WALLS AND BOUNDARY IDENTIFIER----------*/
+   int Available(char ** Maze, int R, int C, int w, int h)
+        {
+        if ( (C > w) || (C < 0)) {return 0;} //check C in bounds
+        if ( (R > h) || (R < 0)) {return 0;} // check R in bounds
+        if ( Maze[R][C] == 'X') {return 0;} // check if wall
+        if ( Maze[R][C] == 'O') {return 0;} // check if past
+        return 1;
+        }
+/*--------------------Recursive Function-----------------------*/
+   void PathFinder(char ** Maze, int R, int C, int w,int h)
+        {
 
-    printf("%d\n", I );
-    printf("%c\n", *Current);
-
-/*-------------------------------------------------------*/
-
-
-
-
-
-
+        if (Available(Maze, R, C,w,h) == 0){return;}//check if closed
+    	Maze[R][C] = 'O'; // MARKING SPOT
+        //Check South
+        if (Available(Maze, R+1, C,w,h) == 1)
+        {
+            printf("S 1\n");
+            PathFinder(Maze,R+1,C,w,h);
+            printf("N 1\n");  
+        }
+    	//check East
+        if (Available(Maze, R, C+1,w,h) == 1)
+        {
+            printf("E 1\n");
+            PathFinder(Maze,R,C+1,w,h);
+            printf("W 1\n");
+        }
+         //Check North
+        if (Available(Maze, R-1, C, w, h) == 1)
+        {
+            printf("N 1\n");
+            PathFinder(Maze,R-1,C,w,h);
+            printf("S 1\n");           
+        }
+    	//Check West;
+    	if (Available(Maze, R, C-1, w, h) == 1)
+        {
+            printf("W 1\n");
+            PathFinder(Maze, R, C-1, w, h);
+            printf("E 1\n");
+    	}
+    return; 
+   	    }
+     PathFinder(Maze, 0, I, w-1, h-1); // CALLING FUNCTION
 }
